10d864204434b75e2797233b3ed349ef4a17d3ab
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-06-27 15:46:30 -0300

    agrego componente cartwidget

---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Item () {
+    return (
+        <h1>Item</h1>
+    );
+}
--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -3,6 +3,7 @@ import "../Styles/NavBar2.css";
 import imagen1 from "../../assets/img/DB-grey.png";
 import imagen2 from "../../assets/img/DigitalBuy-grey.png"
 import { Link, NavLink } from "react-router-dom";
+import { CartWidget } from "./CartWidget";
 
 export function NavBar2() {
   return (
@@ -95,24 +96,7 @@ export function NavBar2() {
             </form>
           </div>
           <div className="col-md-2 d-flex justify-content-around align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-shopping-cart"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-              <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-              <path d="M17 17h-11v-14h-2" />
-              <path d="M6 5l14 1l-1 7h-13" />
-            </svg>
+            <CartWidget/>
           </div>
         </div>
       </nav>
