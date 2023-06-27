58b42dfabe9e351c0516cdd65233c24bc3b87d24
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-06-27 11:23:37 -0300

    armo componentes de item, itemlist, itemlistcontainer

---
+++ DigitalBuy/src/components/ItemList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function ItemList () {
+    return (
+        <h1>ItemList</h1>
+    )
+}
---
+++ DigitalBuy/src/components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function ItemListContainer () {
+    return (
+        <h1>ItemListContainer</h1>
+    )
+}
