e921db84eb213a919a83f15e748e745f855a4a43
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-07-04 10:55:28 -0300

    arreglo dependencias

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { ItemCount } from "./ItemCount";
-import herramientas from "..herramientas.json/";
-import { useEffect, useState } from "react";
+import ItemCount from "./ItemCount";
+import herramientas from "../herramientas.json";
+
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
