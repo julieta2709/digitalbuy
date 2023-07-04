81a251a3d46e3f8e859c33ce9ef05eacd52186bd
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-07-04 09:45:33 -0300

    actualización de componentes

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ItemCount } from "./ItemCount";
+import herramientas from "..herramientas.json/";
+import { useEffect, useState } from "react";
 
-export function ItemListContainer () {
-    return (
-        <h1>ItemListContainer</h1>
-    )
-}
+const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const promesa = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(herramientas);
+      });
+    });
+    promesa.then((data) => {
+      setItems(data);
+    });
+  }, []);
+
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <ItemCount stock={10} />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ItemListContainer;
